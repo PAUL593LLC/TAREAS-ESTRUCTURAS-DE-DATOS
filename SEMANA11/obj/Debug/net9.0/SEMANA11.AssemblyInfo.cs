df2d2e5d0a191ad3ec072760a9afc66865eb51e6
PAUL593LLC <ep.llerenac@uea.edu.ec>
2025-02-27 23:14:37 -0500

    TAREA SEMANA 11

--- SEMANA11/obj/Debug/net9.0/SEMANA11.AssemblyInfo.cs
+++ SEMANA11/obj/Debug/net9.0/SEMANA11.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("SEMANA10")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("SEMANA11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d5d32b2f9dcd3129756438fa92c74ce1d5a3706")]
-[assembly: System.Reflection.AssemblyProductAttribute("SEMANA10")]
-[assembly: System.Reflection.AssemblyTitleAttribute("SEMANA10")]
+[assembly: System.Reflection.AssemblyProductAttribute("SEMANA11")]
+[assembly: System.Reflection.AssemblyTitleAttribute("SEMANA11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
